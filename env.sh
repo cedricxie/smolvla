99718d1b5ae9242c08f178c3a543eefe3d9a4395
Yuesong.xie <cedricxie@gmail.com>
2025-08-30 12:48:47 +0000

    added env.sh

---
+++ env.sh
@@ -0,0 +1,16 @@
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /root/miniconda.sh
+bash /root/miniconda.sh -b -p /root/miniconda3
+export PATH="/root/miniconda3/bin:$PATH"
+source /root/miniconda3/bin/activate
+
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
+conda install ffmpeg=7.1.1 -c conda-forge
+pip install -e .
+pip install -e ".[smolvla]"
+
+# Git config.
+git config --global user.email "cedricxie@gmail.com"
+git config --global user.name "Yuesong.xie"
+
+wandb login 467014dba405f7b5dcd7c1a7c65d75c3afa2f4cb
